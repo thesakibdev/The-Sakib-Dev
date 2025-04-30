7d984fd9981c52cf41b3bdf334a7af04ac5fb9e6
The Sakib Dev <139985754+thesakibdev@users.noreply.github.com>
2025-04-30 20:43:17 +0600

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,24 @@
-import Link from 'next/link'
-import { HiArrowDown } from 'react-icons/hi2'
+import Link from "next/link";
+import { HiArrowDown } from "react-icons/hi2";
 
 const Header = () => {
   return (
-    <header className='padding-container max-container flex gap-4 flexBetween py-4'>
-      <Link href={'/'} className='bold-28'>Port <span className='regular-28 text-secondary'>folio.</span></Link>
-      <Link href={'https://www.fiverr.com/sellers/thesakibdev'} className='btn_dark_rounded px-8 py-2 rounded-full flexCenter gap-4 animate-pulse' target='_blank'>
+    <header className="padding-container max-container flex gap-4 flexBetween py-4">
+      <Link href={"/"} className="bold-28">
+        The Sakib <span className="regular-28 text-secondary">Dev</span>
+      </Link>
+      <Link
+        href={"https://www.fiverr.com/sellers/thesakibdev"}
+        className="btn_dark_rounded px-8 py-2 rounded-full flexCenter gap-4 animate-pulse"
+        target="_blank"
+      >
         <span>Hire me</span>
-        <span className='h-5 w-5 border border-white rounded-full flexCenter animate-bounce'><HiArrowDown/></span>
+        <span className="h-5 w-5 border border-white rounded-full flexCenter animate-bounce">
+          <HiArrowDown />
+        </span>
       </Link>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
